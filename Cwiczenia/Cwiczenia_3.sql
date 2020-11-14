ff6eabde7511f36840cc72cfd5e534980b3e2b6f
gradampl <gr_adam@o2.pl>
2020-11-14 22:18:07 +0100

    Create Cwiczenia_3.sql

---
+++ Cwiczenia/Cwiczenia_3.sql
@@ -0,0 +1,38 @@
+--Dla każdego departamentu wypisać jego nazwę, 
+--imię i nazwisko osoby, która ma w tym dziale 
+--drugie zarobki pod względem wysokości 
+--oraz wysokość tych zarobków
+
+
+CREATE VIEW zad_13 AS
+SELECT a.department_id, b.department_name, a.employee_id, a.first_name, a.last_name, a.salary
+FROM employees a, departments b
+WHERE a.department_id = b.department_id;
+
+
+CREATE VIEW zad_13a AS
+SELECT a.department_id, a.employee_id, a.salary
+FROM zad_13 a
+WHERE a.salary = (SELECT MAX(salary)
+FROM zad_13
+WHERE a.department_id = department_id
+GROUP BY department_id);
+	
+
+
+CREATE VIEW zad_13b AS
+SELECT * FROM employees	p	
+WHERE NOT EXISTS
+(SELECT * FROM zad_13a d 
+WHERE p.department_id = d. department_id
+AND p.employee_id = d.employee_id);
+
+
+CREATE VIEW zad_13c AS
+SELECT *
+FROM zad_13b a
+WHERE a.salary = (SELECT MAX(salary)
+FROM zad_13b
+WHERE a.department_id = department_id
+GROUP BY a.department_id);		
+
