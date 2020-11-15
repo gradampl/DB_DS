8b0b627d5bf3634241822404f4820a6b36fee315
gradampl <gr_adam@o2.pl>
2020-11-15 19:54:11 +0100

    Rozwiązanie lepsze zadania 1

--- Cwiczenia/Cwiczenia_3.sql
+++ Cwiczenia/Cwiczenia_3.sql
@@ -3,6 +3,8 @@
 --drugie zarobki pod względem wysokości 
 --oraz wysokość tych zarobków
 
+-----------------------------------------------------
+--ROZWIĄZANIE PIERWSZE - NIE NAJLEPSZE:
 
 CREATE VIEW zad_13 AS
 SELECT a.department_id, b.department_name, a.employee_id, a.first_name, a.last_name, a.salary
@@ -36,3 +38,23 @@ FROM zad_13b
 WHERE a.department_id = department_id
 GROUP BY a.department_id);		
 
+------------------------------------------------------
+
+-- ROZWIĄZANIE DRUGIE - LEPSZE:
+
+CREATE VIEW zad_13bis AS
+SELECT a.department_id, a.employee_id, a.last_name, a.first_name, a.salary
+FROM employees a
+WHERE a.salary < (SELECT MAX(salary)
+FROM employees
+WHERE a.department_id = department_id
+GROUP BY department_id);
+
+
+CREATE VIEW zad_13abis AS
+SELECT a.department_id, b.department_name, a.employee_id, a.last_name, a.first_name, a.salary
+FROM zad_13bis a, departments b
+WHERE a.salary = (SELECT MAX(salary)
+FROM zad_13bis
+WHERE a.department_id = department_id AND a.department_id = b.department_id
+GROUP BY a.department_id);	
