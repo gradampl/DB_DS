61451969f787d794fce830200f17fbbae8070610
gradampl <gr_adam@o2.pl>
2021-01-27 12:35:44 +0100

    Cw_5 - triggers

--- Cwiczenia/Cw_5.sql
+++ Cwiczenia/Cw_5.sql
@@ -109,3 +109,143 @@ WHEN NO_DATA_FOUND THEN
 RETURN 'No data found!';
 END zad_1f;
 
+
+
+
+--================================================================================
+--================================================================================
+
+
+
+-- Zadanie 2 (wyzwalacze)
+
+-- a)
+-- Stworzyć tabelę archiwum_departamentów (id, nazwa, data_zamknięcia,
+-- ostatni_manager jako imię i nazwisko). Po usunięciu departamentu 
+-- dodać odpowiedni rekord do tej tabeli
+
+
+CREATE TABLE archiwum_departamentow (
+  department_id int,
+  department_name VARCHAR2(20),
+  event_date DATE,
+  manager_first_name VARCHAR2(20),
+  manager_last_name VARCHAR2(20)
+);	   
+	   
+CREATE OR REPLACE TRIGGER do_archiwum
+  AFTER DELETE
+  ON hr.DEPARTMENTS
+  FOR EACH ROW
+  DECLARE
+   imie Varchar(20); nazwisko VARCHAR(20);
+BEGIN
+  SELECT first_name, last_name INTO imie, nazwisko from employees WHERE employee_id = old.manager_id;
+  INSERT INTO archiwum_departamentow (department_id, department_name, event_date, manager_first_name, manager_last_name)
+  VALUES(old.department_id, old.department_name, SYSDATE, imie, nazwisko);
+END;
+
+
+
+-- b)
+-- W razie UPDATE i INSERT na tabeli employees, sprawdzić czy zarobki
+-- łapią się w widełkach 2000 - 26000. Jeśli nie łapią się - zabronić
+-- dodania. Dodać tabelę złodziej(id, USER, czas_zmiany), do której
+-- będą wrzucane logi, jeśli będzie próba dodania, bądź zmiany
+-- wynagrodzenia poza widełki.
+
+CREATE SEQUENCE zlodziej_id_seq
+INCREMENT BY 1
+    START WITH 1
+    MINVALUE 1
+    MAXVALUE 100000
+    CYCLE
+    CACHE 2;
+
+
+CREATE TABLE zlodziej (
+  zlodziej_id int,
+  user_name VARCHAR2(20),
+  event_date DATE  
+);	 
+
+
+CREATE OR REPLACE TRIGGER lap_zlodzieja
+  BEFORE INSERT OR UPDATE 
+  ON EMPLOYEES
+  FOR EACH ROW
+   
+BEGIN
+    IF :new.salary >26000 or :new.salary < 2000
+	 THEN 
+	  INSERT INTO zlodziej (zlodziej_id, user_name, event_date)
+      VALUES(zlodziej_id_seq.nextval, USER, SYSDATE);
+	  RAISE_APPLICATION_ERROR(-20243,'jakistekst');
+	 ELSE
+     DBMS_OUTPUT.PUT_LINE('Alles gut.');
+    END IF;  
+END;
+
+
+
+-- c)
+-- Stworzyć sekwencję i wyzwalacz, który będzie odpowiadał 
+-- za auto_increment w tabeli employees.
+
+
+CREATE SEQUENCE employees_id_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER empl_id_auto_incr 
+BEFORE INSERT ON EMPLOYEES 
+FOR EACH ROW
+
+BEGIN
+  SELECT employees_id_seq.NEXTVAL
+  INTO   :new.employee_id
+  FROM   dual;
+END;
+
+
+
+
+-- d)
+-- Stworzyć wyzwalacz, który zabroni dowolnej operacji
+-- na tabeli JOD_GRADES (INSERT, UPDATE, DELETE)
+
+
+CREATE OR REPLACE TRIGGER prohibit
+  BEFORE INSERT OR UPDATE OR DELETE
+  ON JOB_GRADES
+  FOR EACH ROW
+   
+BEGIN
+    
+	DBMS_OUTPUT.PUT_LINE('Action prohibited!');
+	RAISE_APPLICATION_ERROR(-20243,'jakistekst');   
+    
+END;
+
+
+
+
+
+-- e)
+-- Stworzyć wyzwalacz, który przy próbie zmiany max i min salary
+-- w tabeli jobs zostawia stare wartości.
+
+
+CREATE OR REPLACE TRIGGER remain_old_values
+  BEFORE INSERT OR UPDATE 
+  ON JOBS
+  FOR EACH ROW
+   DECLARE
+    maxi NUMBER; mini NUMBER;
+BEGIN
+    SELECT max_salary, min_salary INTO maxi, mini from JOBS WHERE job_id = ROWID;
+	IF :new.max_salary<>maxi or :new.min_salary <> mini
+	 THEN 
+	  DBMS_OUTPUT.PUT_LINE('Action prohibited!');
+	  RAISE_APPLICATION_ERROR(-20243,'jakistekst');    
+    END IF;  
+END;
+
